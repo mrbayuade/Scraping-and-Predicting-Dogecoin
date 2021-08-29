e1a20604979d8cc9fb9fff4476d5d989debd3c73
Bayu-ade-krisna-s <mrbayuade@gmail.com>
2021-08-29 07:32:55 +0700

    Finishing project.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Scraping-and-Predicting-Dogecoin
-This project describes how to scraping and predicting Dogecoin using library in python (cryptocmd and fbprophet)
+
+First, I want to scrap historical data of Dogecoin in website https://coinmarketcap.com/.
+The data contains open,high,low,and close price of Dogecoin, then I want to predict
+close price of Dogecoin in the future using historical data.
+In this project I use library in python :
+1. cryptocmd : to scrap historical data of a cryptocurrency in https://coinmarketcap.com/
+2. fbprophet : a procedure for forecasting time series data based on an additive model
+               where non-linear trends are fit with yearly, weekly, and daily seasonality,
+               plus holiday effects.
+source : https://pypi.org/
+
+
+# Conclusion
+The historical data of Dogecoin is from  2013-04-28 until 2021-08-27,
+because in this project I scrap the data on 2021-08-29 and data in website
+always update everyday so does the scraper. You can see the codes
+on main.py file.
+Based on results I can conclude that :
+1. Based on trend Dogecoin close price will significantly increase for next year.
+2. Based on weekly I can see the lowest close price is on tuesday and the higgest is on friday.
+3. Based on yearly, the lowest close price is on april and december, and the highest is on may. 
--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-"""
-First, I want to scrap historical data of Dogecoin in website https://coinmarketcap.com/.
-The data contains open,high,low,and close price of Dogecoin, then I want to predict
-close price of Dogecoin in the future using historical data.
-In this project I use library in python :
-1. cryptocmd : to scrap historical data of a cryptocurrency in https://coinmarketcap.com/
-2. fbprophet : a procedure for forecasting time series data based on an additive model
-               where non-linear trends are fit with yearly, weekly, and daily seasonality,
-               plus holiday effects.
-source : https://pypi.org/
-"""
-
 # Data Scraping
 from cryptocmd import CmcScraper
 scraper = CmcScraper("DOGE")
 headers, data = scraper.get_data()
-xrp_json_data = scraper.get_data("json")
+doge_json_data = scraper.get_data("json")
 df = scraper.get_dataframe()
 
 # Data Preparation
@@ -22,12 +10,16 @@ df[["ds","y"]] = df[["Date","Close"]]
 df.describe()
 
 # Data Analysis
-# from fbprophet import Prophet
-# model = Prophet()
-# model = model.fit(df)
-# future   = model.make_future_dataframe(periods=365)
-# forecast = model.predict(future)
-# print(forecast)
+from fbprophet import Prophet
+import matplotlib.pyplot as plt
+model = Prophet()
+model = model.fit(df)
+future = model.make_future_dataframe(periods=365)
+forecast = model.predict(future)
 
-print(df)
-
+# Data Visualization
+ax = (df.plot(x='ds',y='y',figsize=(15,5),title='Actual Vs Forecast'))
+forecast.plot(x='ds',y='yhat',figsize=(15,5),title='Actual vs Forecast', ax=ax)
+model.plot(forecast)
+model.plot_components(forecast)
+plt.show()
